1fbf3694572c264a73766b82269beaf307675a41
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-18 09:57:10 +0300

    commit tree e9ecde87569e8383aaeced62848efaa6f7d5dc1b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        # expected: []
+        #     got: ["hello", "world", "zombies", "pants", "dipper"] === when we write array
+        array.filter do |w|
+            w.split('').sort == word.split('').sort 
+        end
+    #    array.select{|i| i.split('').sort == @word.split('').sort }
+    end
+    
+end
+# listen = Anagram.new('hy')
+# puts listen.match(%w[hello world zombies pants dipper])
